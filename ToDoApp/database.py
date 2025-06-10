d9120b672fda718ef5ff1aaf22bb7fbfbabf7601
Raudra Adak <raudraadak3698@gmail.com>
2025-06-10 11:49:22 +0530

    Updated database.py

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -8,15 +8,10 @@ POSTGRESQL_DATABASE_URL = "postgresql://postgres:1234@localhost/ToDoApplicationD
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 # )
 
-engine = None
-SessionLocal = None
+engine = create_engine(POSTGRESQL_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def init_db(db_url: str = POSTGRESQL_DATABASE_URL):
-    global engine
-    global SessionLocal
-    engine = create_engine(db_url)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Example of how to initialize (call this from main.py or similar)
 # if __name__ == '__main__':
